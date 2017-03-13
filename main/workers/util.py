e383dded47bd270998278c9f3ac7a77cd0a9076d
Peter Sand <petersand123@yahoo.com>
2017-03-13 12:02:18 -0700

    convert log messages to plain strings

--- main/workers/util.py
+++ main/workers/util.py
@@ -6,4 +6,4 @@ from main.resources.resource_util import update_sequence_value, find_resource
 def worker_log(worker_name, message):
     print(worker_name + ': ' + message)
     name = '/system/worker/log'
-    update_sequence_value(find_resource(name), name, datetime.datetime.utcnow(), worker_name + ': ' + message)
+    update_sequence_value(find_resource(name), name, datetime.datetime.utcnow(), str(worker_name + ': ' + message))  # convert unicode to plain string
