2ca4bb0191267c3966a64a01361560bb4bd28899
Peter Sand <petersand123@yahoo.com>
2017-11-06 20:52:53 +1030

    handle case that worker log sequence is missing

--- main/workers/util.py
+++ main/workers/util.py
@@ -6,4 +6,8 @@ from main.resources.resource_util import update_sequence_value, find_resource
 def worker_log(worker_name, message):
     print(worker_name + ': ' + message)
     name = '/system/worker/log'
-    update_sequence_value(find_resource(name), name, datetime.datetime.utcnow(), str(worker_name + ': ' + message))  # convert unicode to plain string
+    log_resource = find_resource(name)
+    if log_resource:
+        update_sequence_value(log_resource, name, datetime.datetime.utcnow(), str(worker_name + ': ' + message))  # convert unicode to plain string
+    else:
+        print('worker log (%s) missing' % name)
