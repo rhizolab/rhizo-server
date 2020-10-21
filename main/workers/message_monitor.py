24423db16493ad2ddb9143a52c48ddf21721e82a
peter <peter>
2020-10-21 08:38:21 -0700

    additional worker code for monitoring MQTT messages

--- main/workers/message_monitor.py
+++ main/workers/message_monitor.py
@@ -1,13 +1,44 @@
+import json
 import gevent
+import paho.mqtt.client as mqtt
+from main.app import db
 from main.workers.util import worker_log
+from main.util import load_server_config
+from main.users.auth import message_auth_token
 
 
 # this worker monitors MQTT messages for ones that need to be acted upon by the server
 def message_monitor():
-    verbose = True
+    server_config = load_server_config()
+    if not 'MQTT_HOST' in server_config:
+        worker_log('message_monitor', 'MQTT host not configured')
+        return
     worker_log('message_monitor', 'starting')
+    server_name = server_config['MQTT_HOST']
+
+    # run this on connect/reconnect
+    def on_connect(client, userdata, flags, rc):
+        if rc:
+            worker_log('message_monitor', 'unable to connect to MQTT broker/server at %s' % server_name)
+        else:
+            worker_log('message_monitor', 'connected to MQTT broker/server at %s' % server_name)
+        client.subscribe('#')  # subscribe to all messages
+
+    # run this on message
+    def on_message(client, userdata, msg):
+        print('MQTT: %s %s' % (msg.topic, msg.payload.decode()))
+
+    # connect and run
+    mqtt_client = mqtt.Client(transport='websockets')
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.username_pw_set('token', message_auth_token(0))
+    mqtt_client.tls_set()  # enable SSL
+    mqtt_client.connect(server_name, 8088)  # using port 8088 for testing
+    mqtt_client.loop_start()
     while True:
         gevent.sleep(60)
+
 
 # if run as top-level script
 if __name__ == '__main__':
