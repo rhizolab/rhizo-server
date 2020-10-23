358933f278f042e1161eddb994219e1c6b3f0792
Steven Grimm <steven@terraformation.com>
2020-10-23 09:58:31 -0700

    Port non-WebSocket-based sequence update tests from client test suite

--- tests/test_resource_api.py
+++ tests/test_resource_api.py
@@ -1,8 +1,12 @@
 import base64
 import random
+from io import BytesIO
 from typing import Union
 
+from PIL import Image
 import pytest
+
+from main.resources.models import Resource
 
 
 class TestResourceOperations:
@@ -60,26 +64,70 @@ class TestResourceOperations:
 
         assert self.client.get(folder_resource_url).status_code == 200
 
-    def test_read_write_text_sequence(self):
+    # Sequences
+
+    def _create_sequence(self, sequence_type: int) -> str:
         folder = '/folder'
         filename = 'test-sequence'
         resources_url = '/api/v1/resources'
-        sequence_url = f'{resources_url}{folder}/{filename}'
+
+        post_params = {'path': folder, 'name': filename, 'type': sequence_type}
+        assert self.client.post(resources_url, data=post_params).status_code == 200
+
+        return f'{resources_url}{folder}/{filename}'
+
+    def _write_then_read_sequence(self, sequence_url: str, data: bytes) -> bytes:
+        """Write a new value to an existing sequence and read it back.
+
+        Return the value as retrieved from the database.
+        """
+        assert self.client.post(
+            sequence_url,
+            data={'data': base64.b64encode(data)}
+        ).status_code == 200
+
+        result = self.client.get(sequence_url)
+        assert result.status_code == 200
+        return result.data
+
+    def test_read_write_text_sequence(self):
+        sequence_url = self._create_sequence(Resource.TEXT_SEQUENCE)
         items = ['ok'.encode(), 'test'.encode()]
 
-        assert self.client.post(resources_url,
-                                data={'path': folder, 'name': filename,
-                                      'type': 2}).status_code == 200
-
         for item in items:
-            assert self.client.post(
-                sequence_url,
-                data={'data': base64.b64encode(item)}
-            ).status_code == 200
+            assert self._write_then_read_sequence(sequence_url, item) == item
 
-            result = self.client.get(sequence_url)
-            assert result.status_code == 200
-            assert self.client.get(sequence_url).data == item
+    def test_update_int_sequence(self):
+        sequence_url = self._create_sequence(Resource.NUMERIC_SEQUENCE)
+        value = random.randint(1, 100)
+        response_content = self._write_then_read_sequence(sequence_url, str(value).encode())
+        assert int(response_content.decode()) == value
+
+    def test_update_float_sequence(self):
+        sequence_url = self._create_sequence(Resource.NUMERIC_SEQUENCE)
+        value = random.randint(1, 100) + .5
+        response_content = self._write_then_read_sequence(sequence_url, str(value).encode())
+        assert round(float(response_content.decode()), 2) == round(value, 2)
+
+    def test_update_image_sequence(self):
+        sequence_url = self._create_sequence(Resource.IMAGE_SEQUENCE)
+
+        # Generate a random image in JPEG format
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        width = 320
+        height = 240
+        image = Image.new('RGB', (width, height))
+        pixel_data = image.load()
+        for y in range(height):
+            for x in range(width):
+                pixel_data[x, y] = (r, g, b)
+        mem_file = BytesIO()
+        image.save(mem_file, 'JPEG')
+        contents = mem_file.getvalue()
+
+        assert self._write_then_read_sequence(sequence_url, contents) == contents
 
 
 class TestSendMessage:
